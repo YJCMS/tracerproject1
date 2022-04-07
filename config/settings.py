73d658978b35dc549df78f0b87590f422a99f7bc
YJCMS <sen9563@gmail.com>
2022-04-07 15:53:09 +0900

    multiple dropzon

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*8+b++%532qbb22^pvns^5iuk)s0!(n7lu8e%a&6)3n5%+4cj)'
+
+import os, json 
+from django.core.exceptions import ImproperlyConfigured 
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') #secrets.json 불러오기
+
+with open(secret_file, 'r') as f: 
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets): #예외 처리
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
